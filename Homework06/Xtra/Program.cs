f78ffaefca9a08510dae702f39b57da742ed0a32
Vladislav Yagello <y.surprise@gmail.com>
2021-10-11 00:10:11 +0300

    Homework06

---
+++ Homework06/Xtra/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace Xtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { -1, 1, 6, 382, -10, 0 };
+            int[] arr2 = { 10, 11, 12, 13, -9 };
+            int[] arr3 = { 1, 2, 3, 0 };
+            WriteLine(GetFirstMissingPositive(arr1));
+            WriteLine(GetFirstMissingPositive(arr2));
+            WriteLine(GetFirstMissingPositive(arr3));
+        }
+
+        static int GetFirstMissingPositive(int[] array)
+        {
+            int i;
+            for (i = 1; array.Contains(i); i++) ;
+            return i;
+        }
+    }
+}
